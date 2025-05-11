309a936ea496e534dea32761ed04480ecc96262a
Kanamu Kato <st002304@m02.kyoto-kcg.ac.jp>
2025-05-12 03:31:11 +0900

    Pressableを利用しボタンを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -4,6 +4,7 @@ import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
 
 import { ImageViewer } from "@/components/image-viewer";
+import { Button } from "@/components/button";
 
 const PlaceholderImage = require("@/assets/images/background-image.png") as ImageSourcePropType;
 
@@ -12,6 +13,10 @@ const App: FC = () => {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="写真を選択" />
+        <Button label="この写真を使用" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -27,6 +32,10 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
 
