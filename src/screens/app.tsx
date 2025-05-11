8ceeb106dd6ebd1d11cb1c8785b274b72f044136
Kanamu Kato <st002304@m02.kyoto-kcg.ac.jp>
2025-05-12 03:22:31 +0900

    Imageコンポーネントを追加して画像を表示

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,15 +1,16 @@
 import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, type ImageSourcePropType, Image, View } from "react-native";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
+
+const PlaceholderImage = require("@/assets/images/background-image.png") as ImageSourcePropType;
 
 const App: FC = () => {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>Hello, World!</Text>
-      <Text style={styles.text}>
-        Open up app.tsx to start working on your app!
-      </Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,11 +21,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff"
-  }
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
 
 registerRootComponent(App);
